be967515d687904ca9a891bb15a3e8b9956f8577
TaiLe1101 <taic21a.th1@gmail.com>
2023-03-08 13:58:29 +0700

    dua file lab06 - 07

---
+++ QLWebDaoTao/Models/Khoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QLWebDaoTao.Models
+{
+    public class Khoa
+    {
+        public string MaKH { set; get; }
+        public string TenKH { set; get; }
+    }
+}
